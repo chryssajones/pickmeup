136d79f8dd1eb15814b23ec9fcf99d162e7f9cef
Chryssa <chryssajones@gmail.com>
2018-01-17 19:42:44 -0800

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './components/map.js';
+import * as firebase from "firebase";
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBJcYs45LZkQlnwei7iSadVqcq-gNbUYT4",
+    authDomain: "pick-me-up-1515730524624.firebaseapp.com",
+    databaseURL: "https://pick-me-up-1515730524624.firebaseio.com",
+    projectId: "pick-me-up-1515730524624",
+    storageBucket: "pick-me-up-1515730524624.appspot.com",
+    messagingSenderId: "1044622095281"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
@@ -8,7 +22,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Pick Me Up!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
