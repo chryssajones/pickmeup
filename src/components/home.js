5a0e1f45d7077b046b5f5363461743a1423e1552
Chryssa <chryssajones@gmail.com>
2018-01-18 19:34:35 -0800

    connected components for map and home

--- src/components/home.js
+++ src/components/home.js
@@ -1,1 +1,15 @@
 // This component will compile the home page and its child components.
+
+import React, { Component } from 'react';
+import Map from './Map';
+
+export default class extends Component {
+
+	render() {
+		return (
+			<div>
+				<Map />
+			</div>
+		)
+	}
+}
